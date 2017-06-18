421ddbec314e0d5a9db8c82ab16d3d8594f6e759
alice.edn@gmail.com <alice.edn@gmail.com>
2017-06-18 09:06:49 +0200

    fonction google map ok

--- application/views/magasins_view.php
+++ application/views/magasins_view.php
@@ -1,35 +1,46 @@
-<!--<!--<div class="container-fluid">-->
-<!--<!--    <div id="abo" class="row col-lg-12">-->
-<!--<!---->
-<!--<!--        -->--><?php ////foreach ($magasins as $magasin) {
-////            ?>
-<!--<!--            <div class="col-lg-6 center-block">-->
-<!--<!--                <div class="blocAbo">-->
-<!--<!--                    <div>-->--><?php ////echo $magasin['nomS']; ?><!--<!--</div>-->
-<!--<!--                    <div>-->--><?php ////echo "Adresse : ".$magasin['rueS']; ?><!--<!--</div>-->
-<!--<!--                    <div>-->--><?php ////echo "Ville : ".$magasin['villeS']; ?><!--<!--</div>-->
-<!--<!--                    <div>-->--><?php ////echo "Directeur : ".$magasin['directeurS']; ?><!--<!--</div>-->
-<!--<!--                </div>-->
-<!--<!--            </div>-->
-<!--<!--        -->--><?php ////} ?>
-<!---->
-<!---->
-<!---->
-<h3 class="text-center col-lg-12">Où sommes-nous ?</h3>
-<div id="map"></div>
+<div class="container-fluid">
+    <div id="abo" class="row col-lg-12">
 
-<script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZZHZJJyA2yhVK-5Vm4qCJaAbDO6aw6fI&callback=initMap">
-</script>
+        <?php foreach ($magasins as $magasin) {
+            ?>
+            <div class="col-lg-6 center-block">
+                <div class="blocAbo">
+                    <div><?php echo $magasin['nomS']; ?></div>
+                    <div><?php echo "Adresse : ".$magasin['rueS']; ?></div>
+                    <div><?php echo "Ville : ".$magasin['villeS']; ?></div>
+                    <div><?php echo "Directeur : ".$magasin['directeurS']; ?></div>
+                </div>
+            </div>
+        <?php } ?>
+        <h3 class="text-center col-lg-12">Où sommes-nous ?</h3>
+        <div id="map" class="col-lg-offset-3 col-lg-6"></div>
+    </div>
+</div>
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZZHZJJyA2yhVK-5Vm4qCJaAbDO6aw6fI&callback=initMap"></script>
 <script>
+    $( document ).ready(function() {
+        initialize();
+
+        <?php
+        foreach ($magasins as $magasin) {
+        $adresse = $magasin['rueS']." ".$magasin['villeS'];
+        $adresse = addslashes( $adresse );
+        ?>
+
+        codeAddress('<?php echo $adresse; ?>')
+
+        <?php
+        }
+        ?>
+    });
+
     var geocoder;
     var map;
     function initialize() {
         geocoder = new google.maps.Geocoder();
-        var latlng = new google.maps.LatLng(-34.397, 150.644);
         var mapOptions = {
-            zoom: 8,
-            center: latlng
+            zoom: 15,
         }
         map = new google.maps.Map(document.getElementById('map'), mapOptions);
     }
@@ -48,19 +59,3 @@
         });
     }
 </script>
-
-<?php
-foreach ($magasins as $magasin) {
-  $adresse = $magasin['rueS']." ".$magasin['villeS'];
-    $adresse = addslashes( $adresse );
-    ?>
-    <input type="button" value="Encode" onclick="codeAddress('<?php echo $adresse; ?>')">
-    <?php
-}
-?>
-
-
-<body onload="initialize()">
-<div id="map" style="width: 320px; height: 480px;"></div>
-
-</body>
